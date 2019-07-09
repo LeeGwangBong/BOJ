4c33615893cb458dbbde0af2342d5878c636d7d7
GwangBong Lee <leegb93@gmail.com>
2019-07-09 17:50:45 +0900

    Add files via upload

--- BOJ_10992/src/Main.java
+++ BOJ_10992/src/Main.java
@@ -1,38 +1,47 @@
 import java.util.Scanner;
 
 public class Main {
+	
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
 		
-		int num = sc.nextInt();
+		int t = sc.nextInt();
 		
-		for (int i = 1; i <= num; i++) {
-			
-			for (int j = num - 1; i <= j; j--) {
-				System.out.print(" ");
-			}
-			if (i < num) {
-				for (int j = 1; j <= 1; j++) {
+		for(int i = 0; i < t; i++) {			
+				if(i == t-1) {
+					
+					for(int j = 0; j < t*2-1; j++) {
 					System.out.print("*");
+					
+					}			
 				}
-			}
-		
-			if (i >= 2 && i!=num) {
-				for (int j = 1; j <i * 2 - 2; j++) {
-					System.out.print(" ");
+				
+				else if(i < t-1) {
+					
+					for(int j = i+1; j < t; j++) {
+						System.out.printf(" ");
+						
+					}
+					
+					for(int j = 0; j < 1; j++) {
+						System.out.printf("*");
+					}
+					
+					for(int j = 0; j < (i*2)-1; j++) {
+						System.out.printf(" ");
+					}
+
 				}
-				for (int j = 1 ; j <= 1; j++) {
-					System.out.print("*");
-				}
-			}
-			
-			if (i == num) {
-				for (int j = 1; j <= num*2-1; j++) {
-					System.out.print("*");
-				}
-			}
-			
-			System.out.println();;
+				
+				if(i > 0 && i < t-1 ) {
+					for(int j = 0; j < 1; j++) {
+						System.out.printf("*");
+					}
+				}	
+			System.out.println("");
 		}
+			
 	}
 }
+
